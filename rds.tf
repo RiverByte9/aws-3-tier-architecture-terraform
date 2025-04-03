3db833a64ce594079815658bf26e4279e3fa47d6
RiverByte9 <kajalsalgar909@gmail.com>
2025-04-03 17:52:10 +0530

    file updated

--- rds.tf
+++ rds.tf
@@ -4,14 +4,14 @@ resource "random_password" "rds_password" {
   special = false
 }
 
-// Store the RDS password in AWS Secrets Manager
+/// Store the RDS password in AWS Secrets Manager
 resource "aws_secretsmanager_secret" "main-rds_password" {
   name = "${var.prefix}-main-rds-password-unique-name"  # Changed name to avoid deletion issue
 }
 
 // Store the secret value
 resource "aws_secretsmanager_secret_version" "rds_password" {
-  secret_id     = aws_secretsmanager_secret.rds_password.id
+  secret_id     = aws_secretsmanager_secret.main-rds_password.id
   secret_string = jsonencode({ password = random_password.rds_password.result })
 }
 
@@ -39,7 +39,7 @@ resource "aws_db_instance" "postgres" {
   engine_version         = "14.12"  # Make sure this version is available in AWS
   instance_class         = "db.t3.micro"
   allocated_storage      = 30
-  username               = "devadmin"  # Changed from "admin" to avoid AWS restriction
+  username               = "devsadmin"  # Changed from "admin" to avoid AWS restriction
   password               = random_password.rds_password.result
   vpc_security_group_ids = [aws_security_group.rds.id]
   db_subnet_group_name   = aws_db_subnet_group.main.name
@@ -53,12 +53,12 @@ resource "aws_db_instance" "postgres" {
 
 // Create a DB subnet group for the RDS instance
 resource "aws_db_subnet_group" "main" {
-  name       = "rds-subnet-grouplatest"
+  name       = "rds-subnet-groupslatest"
   subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
 
   tags = merge(
     local.common_tags,
-    tomap({ "Name" = "${local.prefix}-rds-subnet-grouplatest" })
+    tomap({ "Name" = "${local.prefix}-rds-subnet-groupslatest" })
   )
 }
 
